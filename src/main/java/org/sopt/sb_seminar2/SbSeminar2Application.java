16d5d22eea28486e7b8bc89594a1607336f179e8
Jaeyu Jo <ehay@naver.com>
2018-10-13 14:40:14 +0900

    Sopt seminar2 Init

---
+++ src/main/java/org/sopt/sb_seminar2/SbSeminar2Application.java
@@ -0,0 +1,12 @@
+package org.sopt.sb_seminar2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSeminar2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbSeminar2Application.class, args);
+    }
+}
---
+++ src/main/java/org/sopt/sb_seminar2/api/FirstController.java
@@ -0,0 +1,10 @@
+package org.sopt.sb_seminar2.api;
+
+/**
+ * Created by ehay@naver.com on 2018-10-12
+ * Blog : http://ehay.tistory.com
+ * Github : http://github.com/ehayand
+ */
+
+public class FirstController {
+}
